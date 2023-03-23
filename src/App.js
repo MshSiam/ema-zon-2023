7cb89b60f79c6689c69643e6b81fb1ce4b605e10
MshSiam <siyamkenway@gmail.com>
2023-03-23 12:57:26 +0600

    using router v6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header";
+import About from "./components/about/About";
+import Inventory from "./components/inventory/Inventory";
+import Orders from "./components/orders/Orders";
 import Products from "./components/Products/Products";
 
+import Main from "./layouts/Main";
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main />,
+      children: [
+        {
+          path: "/",
+          loader: () => fetch("products.json"),
+          element: <Products />,
+        },
+        {
+          path: "/orders",
+          loader: () => {
+            return fetch("products.json");
+          },
+          element: <Orders />,
+        },
+        {
+          path: "/inventory",
+          element: <Inventory />,
+        },
+        {
+          path: "/about",
+          element: <About></About>,
+        },
+      ],
+    },
+  ]);
+
   return (
     <div>
-      <Header />
-      <Products />
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
